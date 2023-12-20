864901546aa47c77fa3b39c393262be1f9587fe7
dingxt <dingxt@fosun.com>
2023-12-20 10:56:33 +0800

    fix

--- database_pool/backends/postgresql/creation.py
+++ database_pool/backends/postgresql/creation.py
@@ -1,18 +1,18 @@
-from django.db.backends.postgresql.creation import DatabaseCreation as Psycopg2DatabaseCreation
+from django.db.backends.postgresql.creation import DatabaseCreation as Pg2DatabaseCreation
 
 __all__ = ["DatabaseCreation"]
 
 
-class DatabaseCreation(Psycopg2DatabaseCreation):
+class DatabaseCreation(Pg2DatabaseCreation):
     def _clone_test_db(self, *args, **kw):
         self.connection.dispose()
-        super(DatabaseCreation, self)._clone_test_db(*args, **kw)
+        super()._clone_test_db(*args, **kw)
 
     def create_test_db(self, *args, **kw):
         self.connection.dispose()
-        super(DatabaseCreation, self).create_test_db(*args, **kw)
+        super().create_test_db(*args, **kw)
 
     def destroy_test_db(self, *args, **kw):
         """Ensure connection pool is disposed before trying to drop database."""
         self.connection.dispose()
-        super(DatabaseCreation, self).destroy_test_db(*args, **kw)
+        super().destroy_test_db(*args, **kw)
